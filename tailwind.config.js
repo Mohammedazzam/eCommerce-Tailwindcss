45789e765d0116fd06bb90938cbf5b789cf3620d
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-06-29 02:28:50 +0300

    install + Project processing

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    purge: [],
+    theme: {
+        colors: {
+            white: '#ffffff',
+            orange: {
+                100: '#FEEDD3',
+                200: '#FE7865',
+                300: '#E6604D',
+                400: '#F55C46',
+            },
+            blue: {
+                100: '#F5F8FC',
+                200: '#EFEDFE',
+                300: '#5BD5E6'
+            },
+            green: {
+                100: '#FFFBF8',
+                200: '#E8FBF7',
+                300: '#5BF6B0'
+            }
+        },
+        extend: {
+
+        },
+    },
+    variants: {},
+    plugins: [],
+}
