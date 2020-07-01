11459caae0cdeae45ab474f49ca24d2ce3970146
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-07-01 12:29:34 +0300

    6 finish

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function(){
     $('.slider').slick({
-        arrows: false,
-        dots: true,
-        appendDots: '.slider-dots',
-        fade: true
+        arrows:false,
+        dots:true,
+        appendDots:'.slider-dots',
+        fade:true
+    });
+
+    $('.slider2').slick({
+        arrows:false,
+        dots:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        centerMode:true,
+        autoplay:true,
+        appendDots:'.slider2-dots',
+        // fade:true
     });
 });
 
@@ -12,8 +23,8 @@ const triggers = document.querySelectorAll('.menu-trigger');
 
 console.log(triggers);
 
-for (let i = 0; i < triggers.length; i++) {
-    triggers[i].addEventListener('click', function(e) {
+for (let i = 0; i < triggers.length; i++){
+    triggers[i].addEventListener('click', function(e){
         e.preventDefault();
         this.classList.toggle('is-open');
     });
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    plugins: [
+    plugins:[
         require('tailwindcss'),
         require('autoprefixer')
+
     ]
 }
